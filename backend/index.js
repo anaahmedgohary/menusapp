78fe02625ca9850872158435f8b9f81cafc84a89
anaahmedgohary <gogoahmed13@gmail.com>
2022-11-01 01:31:10 +0200

    AP08

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,19 @@ app.use(bodyParser.json());
 
 // app.use('/', routerHandler);
 
+// CORS Fix
+    var allowCrossDomain = function (req, res, next)
+    {
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    };
+
+    app.use(allowCrossDomain);
+// CORS Fix end
+
+
 
 
 
